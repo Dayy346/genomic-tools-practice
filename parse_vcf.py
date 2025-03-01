88a315d7f7e7ec8f28f428467b646b0d80fd66b7
Dayyan Hamid <dayyan6093@gmail.com>
2025-02-28 23:04:47 -0500

    trying vcfpy

--- parse_vcf.py
+++ parse_vcf.py
@@ -1,9 +1,9 @@
-import vcf
+import vcfpy
 
 def parse_vcf(file_path):
-    vcf_reader = vcf.Reader(open(file_path, 'r'))
-    for record in vcf_reader:
-        print(f"Chromosome: {record.CHROM}, Position: {record.POS}, ID: {record.ID}, Ref: {record.REF}, Alt: {record.ALT}")
+    reader = vcfpy.Reader.from_path(file_path)
+    for record in reader:
+        print(f"Chromosome: {record.CHROM}, Position: {record.POS}, Ref: {record.REF}, Alt: {record.ALT}")
 
 if __name__ == "__main__":
     parse_vcf("example-4.1.vcf")
